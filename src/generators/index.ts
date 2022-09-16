98c1b1bc2c24b2c5f6ccafda2dd6772abb47dca2
MidnightTinge <79157320+MidnightTinge@users.noreply.github.com>
2022-09-15 22:09:34 -0500

    Fix nully observer issues

--- src/generators/index.ts
+++ src/generators/index.ts
@@ -7,7 +7,13 @@ import SColors from './impl/styles/Colors';
 import SShadows from './impl/styles/Shadows';
 import STypography from './impl/styles/Typography';
 
+function noop() {
+  // stub
+}
+
 export async function buildAssetMap(RunKey: string, observer: ProgressObserver): Promise<AssetMap> {
+  observer = observer ?? noop;
+
   const map: AssetMap = {} as any; // as any since we're building this piecemeal
 
   map.colors = await SColors.generate(RunKey, map, observer);
@@ -18,6 +24,8 @@ export async function buildAssetMap(RunKey: string, observer: ProgressObserver):
 }
 
 export async function generate(RunKey: string, observer?: ProgressObserver) {
+  observer = observer ?? noop;
+
   const assetMap = await buildAssetMap(RunKey, observer);
 
   await PColors.generate(RunKey, assetMap, observer);
